d55889cb6dffce2466d4c4381d1a1098132b8ed3
EricaMarroquin <ericamarroquin03@gmail.com>
2021-08-27 10:22:10 -0700

    WIP: add cost function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function Pizza() {
   this.size = "small";
-  this.toppings = {};
+  this.toppings = [];
 }
 
-const toppings = {
+const toppingsMenu = {
   cheese: 0,
   pepperoni: 0.5,
   pineapple: 0.5,
@@ -12,6 +12,25 @@ const toppings = {
   peppers: 0.25,
 }
 
-Pizza.prototype.totalCost = function() {
-
+Pizza.prototype.totalCost = function(size, chosenToppings) {
+  let cost = 5;
+  const toppingKeys = Object.keys(toppingsMenu);
+  for (let i = 0; i < chosenToppings.length; i++) {
+    console.log(i);
+    for (j = 0; j < toppingKeys.length; j++) {
+      console.log(j);
+      if (i === toppingKeys[j]) {
+        cost += toppingsMenu.toppingKeys[j];
+      }
+    }
+  }
+  return cost;
 }
+
+  if (size === "medium") {
+    cost += 1
+  } else if (size === "large") {
+    cost += 2
+  } else if (size === "XL") {
+    cost += 3
+  }
